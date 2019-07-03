2a19eed6fa7182cc565fb70f3fe3a8b748667eba
Konstantin Rubinov <rubinovk@users.noreply.github.com>
2019-07-03 11:08:37 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,12 +1,29 @@
-# vuetify-text-field
-Vuetify text field
+# text-field-test
 
-In IE11 text field value is not completely reset after the browser page refresh (reproducible every other time). It disappears on input events. See how it overlays with the label. 
+## Project setup
+```
+npm install
+```
 
-Link to reproduce:
-https://vuetify-text-field.azurewebsites.net/
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
 
-![BUG](/text-field-refresh-ie.PNG?raw=true "Title")
+### Compiles and minifies for production
+```
+npm run build
+```
 
+### Run your tests
+```
+npm run test
+```
 
-Any ideas? Is it a known bug? Thank you
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-form ref="form" v-model="accept" lazy-validation>
+        <v-layout row justify-center class="mt-5" wrap>
+          <v-flex xs12 md5>
+            <v-text-field
+              label="Name"
+              :value="generalApplicantInfo.name"
+              @change="v => generalApplicantInfo.name = v"
+              :rules="[rules.required]"
+              required
+            ></v-text-field>
+            <v-text-field
+              label="Surname"
+              :value="generalApplicantInfo.surName"
+              @change="v => generalApplicantInfo.surName = v"
+              :rules="[rules.required]"
+              required
+            ></v-text-field>
+          </v-flex>
+        </v-layout>
+      </v-form>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data: () => ({
+    accept: true,
+    generalApplicantInfo: {
+      gender: '',
+      name: '',
+      surName: ''
+    },
+    rules: {
+      required: value => !!value || 'field is required'
+    }
+  })
+}
+</script>
